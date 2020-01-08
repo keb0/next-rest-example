ff8088a2ba8456f8186e9ad1ee6fad189f074c57
k <devs079@yahoo.co.jp>
2020-01-09 00:05:25 +0900

    Add footer, Add styled

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://nextjs.org/">NEXT.JS</a>
+    </footer>
+  )
+}
